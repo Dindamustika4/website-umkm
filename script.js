85d45d8fc331454c6aec214c1177a1c656463e4b
Dindamustika4 <dindamustikaandini@gmail.com>
2026-09-07 22:11:46 +0700

    feat: buat landing page awal UMKM

---
+++ script.js
@@ -0,0 +1,6 @@
+const promoButton = document.querySelector("#promoButton");
+
+promoButton.addEventListener("click", () => {
+    promoButton.textContent = "Promo Beli Mi Bangladesh free Kopi gula aren!";
+    console.log("Promo Kopi Kennagan berhasil ditampilkan.");
+});
