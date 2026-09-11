f24307a7ad97173353c6d0ddf49f806da66247f3
Dindamustika4 <dindamustikaandini@gmail.com>
2026-09-11 12:54:45 +0700

    feat: tambah informasi lokasi UMKM

--- script.js
+++
@@ -1,6 +0,0 @@
-const promoButton = document.querySelector("#promoButton");
-
-promoButton.addEventListener("click", () => {
-    promoButton.textContent = "Promo Beli Mi Bangladesh free Kopi gula aren!";
-    console.log("Promo Kopi Kennagan berhasil ditampilkan.");
-});
